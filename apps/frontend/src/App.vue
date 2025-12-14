21140258b8d9e4685faeefc514c13ceed6295261
ReniX99 <oleg.burkov99@gmail.com>
2025-12-14 16:25:05 +0300

    feat-client: init frontend app

---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <header class="border-b border-b-black">
+      <div class="h-19 max-w-300 mx-auto flex justify-between items-center">
+        <h1 class="font-medium tracking-widest text-[24px] uppercase">Giga Cosmic</h1>
+        <nav>
+          <ul class="flex gap-14 text-[16px] font-medium">
+            <li class="cursor-pointer">Секция 1</li>
+            <li class="cursor-pointer">Секция 2</li>
+            <li class="cursor-pointer">Секция 3</li>
+            <li class="cursor-pointer">Секция 4</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </main>
+</template>
+
+<style>
+  body {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 16px;
+  }
+</style>
